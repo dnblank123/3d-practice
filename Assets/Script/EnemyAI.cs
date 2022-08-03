29c3b710732bde7897f656309ad5a079ae6e8352
dnblank123 <67945097+dnblank123@users.noreply.github.com>
2022-08-03 17:30:56 +0800

    more assets

--- Assets/Script/EnemyAI.cs
+++ Assets/Script/EnemyAI.cs
@@ -78,8 +78,11 @@ public class EnemyAI : MonoBehaviour
     {
         //Make sure enemy doesn't move
         agent.SetDestination(transform.position);
-
-        transform.LookAt(player);
+        
+        Vector3 direction = (player.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime*5f);
+ 
 
     }
     private void ResetAttack()
