847a6ce002f00576d0cc58b0c9de25f22327ac90
dnblank123 <67945097+dnblank123@users.noreply.github.com>
2022-08-03 22:21:48 +0800

    change player model + enemy animation
    TODO:
    rocks
    foliage
    house
    spawn enemy

--- Assets/Script/EnemyAI.cs
+++ Assets/Script/EnemyAI.cs
@@ -26,11 +26,28 @@ public class EnemyAI : MonoBehaviour
     //States
     public float sightRange, attackRange;
     public bool playerInSightRange, playerInAttackRange;
+    //animation
+    private Animator animator;
+    int isRunHash;
+    int isRunBackHash;
+
+    int isRunLeftHash;
+    int isRunRightHash;
+    int isAttackHash;
 
     private void Awake()
     {
-        player = GameObject.Find("HumanMale").transform;
+        player = GameObject.Find("manlapulapu").transform;
         agent = GetComponent<NavMeshAgent>();
+    }
+    private void Start() 
+    {
+        animator = GetComponent<Animator>();
+        isRunHash = Animator.StringToHash("isRun");
+        isRunBackHash = Animator.StringToHash("isRunBack");
+        isRunLeftHash = Animator.StringToHash("isRunLeft");
+        isRunRightHash = Animator.StringToHash("isRunRight");
+        isAttackHash = Animator.StringToHash("isAttack");
     }
 
     private void Update()
@@ -42,15 +59,16 @@ public class EnemyAI : MonoBehaviour
         if (!playerInSightRange && !playerInAttackRange) Patroling();
         if (playerInSightRange && !playerInAttackRange) ChasePlayer();
         if (playerInAttackRange && playerInSightRange) AttackPlayer();
+
     }
 
     private void Patroling()
     {
         if (!walkPointSet) SearchWalkPoint();
-
+            
         if (walkPointSet)
             agent.SetDestination(walkPoint);
-
+           
         Vector3 distanceToWalkPoint = transform.position - walkPoint;
 
         //Walkpoint reached
@@ -72,22 +90,44 @@ public class EnemyAI : MonoBehaviour
     private void ChasePlayer()
     {
         agent.SetDestination(player.position);
+        animator.SetBool("isRun", true);
+
     }
 
     private void AttackPlayer()
     {
+        bool isAttack = animator.GetBool("isAttack");
         //Make sure enemy doesn't move
         agent.SetDestination(transform.position);
-        
+        animator.SetBool("isRun", false);
+
         Vector3 direction = (player.position - transform.position).normalized;
         Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime*5f);
  
+        if (!alreadyAttacked)
+        {
+            ///Attack code here
+            animator.SetBool("isAttack", false);
+            Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+            rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
+            rb.AddForce(transform.up * 8f, ForceMode.Impulse);
+            ///End of attack code
+           
 
+     
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+            animator.SetBool("isAttack", true);
+        }
+
+        
     }
     private void ResetAttack()
     {
-        alreadyAttacked = false;
+        
+        alreadyAttacked = false;  
+
     }
 
     public void TakeDamage(int damage)
---
+++ Assets/Script/EnemyAnimation.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAnimation : MonoBehaviour
+{
+ private Animator animator;
+    int isRunHash;
+    int isRunBackHash;
+
+    int isRunLeftHash;
+    int isRunRightHash;
+    int isJumpHash;
+    int isAttackHash;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        isRunHash = Animator.StringToHash("isRun");
+        isRunBackHash = Animator.StringToHash("isRunBack");
+        isRunLeftHash = Animator.StringToHash("isRunLeft");
+        isRunRightHash = Animator.StringToHash("isRunRight");
+        isJumpHash = Animator.StringToHash("isJump");
+        isAttackHash = Animator.StringToHash("isAttack");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerAnimate();
+
+    }
+
+    private void PlayerAnimate()
+    {
+        bool isRun = animator.GetBool(isRunHash);
+        bool isRunBack = animator.GetBool(isRunBackHash);
+        bool isRunLeft = animator.GetBool(isRunLeftHash);
+        bool isRunRight = animator.GetBool(isRunRightHash);
+        bool isJump = animator.GetBool(isJumpHash);
+        bool isAttack = animator.GetBool(isAttackHash);
+
+
+        bool ForwardRun = Input.GetKey("w");
+        bool BackRun = Input.GetKey("s");
+        bool LeftRun = Input.GetKey("a");
+        bool RightRun = Input.GetKey("d");
+        bool Jumped = Input.GetKey("space");
+        bool Attack = Input.GetMouseButton(0);
+
+
+        //forward 
+        if(!isRun && ForwardRun)
+        {
+            animator.SetBool("isRun", true);
+        }
+        //back to idle
+        if(isRun && !ForwardRun)
+        {
+            animator.SetBool("isRun", false);
+
+        }
+        //back
+        if(!isRunBack && BackRun)
+        {
+            animator.SetBool("isRunBack", true);
+
+        }
+        //back to idle
+        if(isRunBack && !BackRun)
+        {
+            animator.SetBool("isRunBack", false);
+
+        }
+        //left
+        if(!isRunLeft && LeftRun)
+        {
+            animator.SetBool("isRunLeft", true);
+        }
+        //back to idle
+        if(isRunLeft && !LeftRun)
+        {
+            animator.SetBool("isRunLeft", false);
+
+        }
+        // right
+        if(!isRunRight && RightRun)
+        {
+            animator.SetBool("isRunRight", true);
+
+        }
+        //back to idle
+        if(isRunRight && !RightRun)
+        {
+            animator.SetBool("isRunRight", false);
+        }
+        //jump
+        if(!isJump && Jumped)
+        {
+            animator.SetBool("isJump", true);
+        }
+        if(isJump && !Jumped)
+        {
+            animator.SetBool("isJump", false);
+        }
+        if(!isAttack && Attack)
+        {
+            animator.SetBool("isAttack", true);
+
+        }
+        if(isAttack && !Attack)
+        {
+            animator.SetBool("isAttack", false);
+
+        }
+    }
+}
