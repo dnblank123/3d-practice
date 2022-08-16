9707cb74689b37972a540ed1da9becdce51233f8
dnblank123 <67945097+dnblank123@users.noreply.github.com>
2022-08-17 01:36:39 +0800

    player prefs to save settings

--- Assets/Script/SettingsMenu.cs
+++ Assets/Script/SettingsMenu.cs
@@ -3,16 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
-    //public Dropdown resoDropdown;
+    public Slider volSlider;
     public TMPro.TMP_Dropdown resoDropdown;
     Resolution[] resolutions;
+    public Toggle fullScreenToggle;
+    public TMPro.TMP_Dropdown qualityDropDown;
 
+    //prefs 
+    const string prefName = "optionValue";
+    const string resName = "resoOption";
+
+    private bool isFullscreen = false;
+
+    private int screenInt;
+    
+    private void Awake()
+    {
+        screenInt = PlayerPrefs.GetInt("togglestate");
+
+        if(screenInt == 1)
+        {
+            isFullscreen = true;
+            fullScreenToggle.isOn = true;
+            
+        } else {
+            fullScreenToggle.isOn = false;
+        }
+
+        resoDropdown.onValueChanged.AddListener(new UnityAction<int>(index =>
+        {
+            PlayerPrefs.SetInt(resName, resoDropdown.value);
+            PlayerPrefs.Save();
+        }
+        ));
+        qualityDropDown.onValueChanged.AddListener(new UnityAction<int>(index =>
+        {
+            PlayerPrefs.SetInt(prefName, qualityDropDown.value);
+            PlayerPrefs.Save();
+        }
+        ));
+    }
     private void Start() 
     {
+        volSlider.value = PlayerPrefs.GetFloat("MVolume", 1f);
+        audioMixer.SetFloat("volume", PlayerPrefs.GetFloat("MVolume"));
+        qualityDropDown.value = PlayerPrefs.GetInt(prefName, 3);
+
+
         resolutions = Screen.resolutions;
         resoDropdown.ClearOptions();
         List<string> options = new List<string>();
@@ -32,7 +74,8 @@ public class SettingsMenu : MonoBehaviour
             
         }
         resoDropdown.AddOptions(options);
-        resoDropdown.value = currentResolutionIndex;
+        //resoDropdown.value = currentResolutionIndex;
+        resoDropdown.value = PlayerPrefs.GetInt(resName, currentResolutionIndex);
         resoDropdown.RefreshShownValue();
 
     }
@@ -58,5 +101,14 @@ public class SettingsMenu : MonoBehaviour
     public void SetFullscreen(bool isFullscreen)
     {
         Screen.fullScreen = isFullscreen;
+        if(isFullscreen == false)
+        {
+            PlayerPrefs.SetInt("togglestate", 0);
+
+        } else {
+            isFullscreen = true;
+            PlayerPrefs.SetInt("togglestate", 1);
+
+        }
     }
 }
